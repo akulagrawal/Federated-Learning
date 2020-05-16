daa83042512896fcfcde83813faecd2e8d18cc35
Akul Agrawal <akulagrawal@rediffmail.com>
2020-05-16 17:08:38 +0530

    Add final results

---
+++ My Papers/Final Paper/Results/obervations.py
@@ -0,0 +1,39 @@
+import pickle
+import pandas as pd
+
+for acc in [0.90, 0.91, 0.92, 0.93, 0.94]:
+
+	for type in ["","_hybrid"]:
+		for n_agents in range(1,4):
+			filename = "results_"+str(n_agents)+str(type)+".pkl"
+			with open(filename, "rb") as f:
+				train_loss, train_acc, test_acc, test_loss, clients, agents, times = pickle.load(f)
+
+			print(filename)
+
+			timet = []
+			avg = []
+			for i in range(len(test_acc)):
+				sum = 0.0
+				if i+19 < len(test_acc):
+					for j in range(20):
+						sum = sum + test_acc[i+j]
+					if times[i] > 290000.0:
+						break
+					avg.append(sum/20.0)
+					timet.append(times[i])
+					i=i+20
+
+			for i in range(len(avg)):
+				if avg[i] >= acc:
+					print(str(acc)+", "+str(timet[i])+", "+str(i))
+					break
+
+	print("Centralized")
+
+	avg = pd.read_csv("cen_0.2_test_accuracy.csv", header = None)
+	timet = pd.read_csv("cen_0.2_times.csv", header = None)
+	for i in range(len(avg)):
+		if avg[0][i] >= acc:
+			print(str(acc)+", "+str(timet[0][i])+", "+str(i))
+			break
